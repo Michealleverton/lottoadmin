a87c85a9419eab021dd2df4a0f4612c93273d91b
Micheal Leverton <levertonmicheal@gmail.com>
2022-12-31 20:00:51 -0500

    sdfsd

--- src/components/ContractFFcopy.js
+++ src/components/ContractFFcopy.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import './contractoverview.css'
+import './Popup.css'
 
 function ContractFFcopy() {
+
+  const texttocopy = "0xB954343f87e77B5e846446BB1167C1e5cf35DF2C"
+
+  const copytoclipboard = () => {
+    navigator.clipboard.writeText(texttocopy)
+  }
+
+  function click() {
+    let copyText = document.querySelector(".copycontractaddress");
+    copyText.classList.add("active");
+    setTimeout(function () {
+      copyText.classList.remove("active");
+    }, 2000)
+  }
+
   return (
     <div className="copycontractaddress">
       <div className='addresstext'>
-      <h6><p id="text">Contract : 0xAD031564418353D49406BD1B3410bA2FbbaCd1C5</p></h6>
+        <h6>Contract : 0xB954343f87e77B5e846446BB1167C1e5cf35DF2C</h6>
+      </div>
+      <button value="copy" onClick={() => { copytoclipboard(); click(); }} id="copy-text-btn"><i className="fa-regular fa-copy"></i></button>
     </div>
-    <button id="copy-text-btn"><i className="fa-regular fa-copy"></i></button>
-</div>
   )
 }
 
--- src/components/ContractTDcopy.js
+++ src/components/ContractTDcopy.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import './contractoverview.css'
+import './Popup.css'
 
 function ContractTDcopy() {
+
+  const texttocopy = "0xECC5Eed8054F0A75118619203760D28D1a85b4e5"
+
+  const copytoclipboard = () => {
+    navigator.clipboard.writeText(texttocopy)
+  }
+
+  function click() {
+    let copyText = document.querySelector(".copycontractaddress");
+    copyText.classList.add("active");
+    setTimeout(function () {
+      copyText.classList.remove("active");
+    }, 1500)
+  }
+
   return (
     <div className="copycontractaddress">
       <div className='addresstext'>
-      <h6><p id="text">Contract : 0xAECc97279cE7B664561C1C0525bf6B159DB295C6</p></h6>
+        <h6>Contract : 0xECC5Eed8054F0A75118619203760D28D1a85b4e5</h6>
+      </div>
+      <button value="copy" onClick={() => { copytoclipboard(); click(); }} id="copy-text-btn"><i className="fa-regular fa-copy"></i></button>
     </div>
-    <button id="copy-text-btn"><i className="fa-regular fa-copy"></i></button>
-</div>
   )
 }
 
